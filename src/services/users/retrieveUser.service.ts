98e50eca89357d6838b62dcfba0f5242387e7826
bielssinho <bielnmjr@gmail.com>
2023-05-25 16:35:41 -0300

    fix: retrieve

--- src/services/users/retrieveUser.service.ts
+++ src/services/users/retrieveUser.service.ts
@@ -2,12 +2,12 @@ import { Repository } from 'typeorm'
 import { AppDataSource } from '../../data-source'
 import { TUserResponse, TUserResponseWhitContacts } from '../../interfaces/users.interfaces'
 import User from '../../entities/user.entitie'
-import { userSchemaResponseWhitContacts } from '../../schemas/users.schema'
+import { userSchemaResponse, userSchemaResponseWhitContacts } from '../../schemas/users.schema'
 
 const retrieveUserService = async (idUser: string): Promise<TUserResponseWhitContacts> => {
     const usersRepository: Repository<User> = AppDataSource.getRepository(User)
 
-    const listUsers: Array<User> = await usersRepository.find({
+    const listUsers: User | null = await usersRepository.findOne({
         where: {
             id: idUser
         },
