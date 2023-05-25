84763ff001b4a641e7370eed77ab9d8aeb70acad
bielssinho <bielnmjr@gmail.com>
2023-05-25 14:49:58 -0300

    fix: schema contact

--- src/schemas/contacts.schema.ts
+++ src/schemas/contacts.schema.ts
@@ -5,12 +5,12 @@ const contactSchema = z.object({
     contactName: z.string().min(3, 'O nome deve ter no minimo 3 caracteres.').max(120, 'O nome deve ter no máximo 120 caracteres.'),
     contactEmail: z.string().email('O email deve ser um formato válido.'),
     contactCellphone: z.string().min(10).max(12,'O numero deve ter no máximo 12 caracteres.'),
-    createAt: z.string()
+    contactCreateAt: z.string()
 })
 
 const contactSchemaRequest = contactSchema.omit({
     id: true,
-    createAt: true
+    contactCreateAt: true
 })
 
 const multiContactsSchemaResponse = contactSchema.array()
