adfa9656fc733560d44a71de2730967e794419c0
bielssinho <bielnmjr@gmail.com>
2023-05-25 14:42:13 -0300

    feat: schema contacts

--- src/schemas/contacts.schema.ts
+++ src/schemas/contacts.schema.ts
@@ -5,7 +5,7 @@ const contactSchema = z.object({
     contactName: z.string().min(3, 'O nome deve ter no minimo 3 caracteres.').max(120, 'O nome deve ter no máximo 120 caracteres.'),
     contactEmail: z.string().email('O email deve ser um formato válido.'),
     contactCellphone: z.string().min(10).max(12,'O numero deve ter no máximo 12 caracteres.'),
-    createAt: z.date()
+    createAt: z.string()
 })
 
 const contactSchemaRequest = contactSchema.omit({
